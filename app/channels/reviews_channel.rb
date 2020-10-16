060b6b68801920dc4b9f861819d9c5383f6c0542
Rahul Verma <rahulv@live.ca>
2020-10-16 01:44:16 -0400

    Remove the non-initial app setup for channels

--- app/channels/reviews_channel.rb
+++
@@ -1,9 +0,0 @@
-class ReviewsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "reviews_channel"
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -6,7 +6,6 @@ module Api::V1
       review = kitty.reviews.new(review_params)
       if review.save
         json = ReviewSerializer.new(review).serialized_json
-        ActionCable.server.broadcast 'reviews_channel', content: json
         render json: json
       else
         render json: { error: review.errors.messages }, status: 422
